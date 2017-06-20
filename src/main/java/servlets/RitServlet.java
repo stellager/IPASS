58de4bdc82394ff9282668b101907fa81a5489ba
Tim <stellager@users.noreply.github.com>
2017-06-21 00:34:26 +0200

    ss
    ss

--- src/main/java/servlets/RitServlet.java
+++ src/main/java/servlets/RitServlet.java
@@ -38,7 +38,7 @@ if(ritOpgeslagen.equals("SUCCESS"))   //On success, you can display a message to
 {
 	 Cookie RitCookie = new Cookie("ritopgeslagen",ritnaam);
 	 response.addCookie(RitCookie);
-request.getRequestDispatcher("/sign_up_succesful.html").forward(request, response);
+request.getRequestDispatcher("/planner.html").forward(request, response);
 }
 else   //On Failure, display a meaningful message to the User.
 	
