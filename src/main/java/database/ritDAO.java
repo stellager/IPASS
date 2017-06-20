078c89f3984e41e0ee9cf6cdb3ac038d35190977
Tim <stellager@users.noreply.github.com>
2017-06-20 23:01:33 +0200

    te

--- src/main/java/database/ritDAO.java
+++ src/main/java/database/ritDAO.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class ritDAO extends BaseDAO{
 public String saveRit(String beginpunt, String eindpunt, int afstand, int duur,String email, String date, String ritnaam, String tijd)
 {
-	
+	String querydan = "empty";	
 	 String query = "INSERT INTO ritten values (?,?,?,?,?,?,?,?)";
 
 	 try (Connection con = super.getConnection()) {
@@ -24,7 +24,7 @@ public String saveRit(String beginpunt, String eindpunt, int afstand, int duur,S
 	 pstmt.setString(6, date);
 	 pstmt.setString(7, ritnaam);
 	 pstmt.setString(8, tijd);
-	 
+	 querydan=pstmt.toString();
 	 int i= pstmt.executeUpdate();
 	 if (i!=0)  //Just to ensure data has been inserted into the database
 	 return "SUCCESS"; 
@@ -33,6 +33,6 @@ public String saveRit(String beginpunt, String eindpunt, int afstand, int duur,S
 	 {
 	 e.printStackTrace();
 	 }
-	 return "Oops.. Something went wrong there..!";  // On failure, send a message from here.
+	 return "Oops.. Something went wrong there..!"+querydan;  // On failure, send a message from here.
 	 }
 	 }
