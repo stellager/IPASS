ec3d28072e40702d498e4b0a247b13d8acbcef21
Tim <stellager@users.noreply.github.com>
2017-06-20 14:02:17 +0200

    a

--- src/main/java/database/loginDAO.java
+++ src/main/java/database/loginDAO.java
@@ -22,7 +22,9 @@ public class loginDAO extends BaseDAO{
 		
 		 if (count!=0)  //Just to ensure data has been inserted into the database
 		 return "SUCCESS"; 
-		
+		 else{
+			 return "NOUSER";
+		 }
 		 
 		 }
 		 catch(SQLException e)
