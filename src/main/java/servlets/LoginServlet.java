e3f83d472dfc657aaaa203658b9c75c3b763fabf
Tim <stellager@users.noreply.github.com>
2017-06-20 14:41:55 +0200

    ss

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -12,10 +12,13 @@ import javax.servlet.http.HttpSession;
 import database.registerDAO;  
 
 public class LoginServlet extends HttpServlet {  
+	private static final long serialVersionUID = 1L;
+	  public LoginServlet() {
+	    	}
     protected void doPost(HttpServletRequest request, HttpServletResponse response)  
                     throws ServletException, IOException {  
     	
-      
+    	
           
         String email=request.getParameter("email");  
         String wachtwoord=request.getParameter("wachtwoord");  
