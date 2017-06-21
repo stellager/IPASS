797bca5603baeec40c877c5d1c868c2374491b83
Tim <stellager@users.noreply.github.com>
2017-06-21 21:58:45 +0200

    aa

--- src/main/java/database/dataDAO.java
+++ src/main/java/database/dataDAO.java
@@ -45,7 +45,7 @@ public class dataDAO extends BaseDAO{
 			 	job.add("ritnaam", resultset.getString(1));
 			 	job.add("keergereden",Integer.toString(resultset.getInt(4)));
 			 	job.add("totaalkm", String.valueOf(round));
-			 	job.add("totaaltijd", "Totale tijd: "+uren+" uur en "+resultset.getInt(2)+" minuten.");
+			 	job.add("totaaltijd", "Totale tijd: "+uren+" uur en "+resultset.getInt(2)+raw+" minuten.");
 			 	
 		    	jab.add(job);
 		    	
