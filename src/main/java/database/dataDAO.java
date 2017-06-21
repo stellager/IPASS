12c635b543c195813baf8427f1eb3fd3b9bc4e51
Tim <stellager@users.noreply.github.com>
2017-06-21 23:07:56 +0200

    s

--- src/main/java/database/dataDAO.java
+++ src/main/java/database/dataDAO.java
@@ -26,8 +26,8 @@ public class dataDAO extends BaseDAO{
 		LocalDate volgendeDate = localDate.plusMonths(1);
 		
 		
-		 String querycurrmonth = dtf.format(localDate)+"-01";
-		 String querynextmonth= dtf.format(volgendeDate)+"-01";
+		 String querycurrmonth = dtf.format(localDate).toString()+"-01";
+		 String querynextmonth= dtf.format(volgendeDate).toString()+"-01";
 		 
 		 try (Connection con = super.getConnection()) {
 
