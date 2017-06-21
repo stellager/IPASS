f42018211d9d455348797b5e3d1b1f387cb73ded
Tim <stellager@users.noreply.github.com>
2017-06-21 16:57:06 +0200

    major

---
+++ src/main/java/database/dataDAO.java
@@ -0,0 +1,61 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+
+public class dataDAO extends BaseDAO{
+	JsonArrayBuilder jab = Json.createArrayBuilder();
+	public JsonArray getData(String email)
+	{
+		String query = "SELECT ritnaam , SUM(duur) AS som , SUM(afstand), count(ritnaam) FROM ritten where email = '?' GROUP BY ritnaam ORDER BY som DESC";
+
+		 try (Connection con = super.getConnection()) {
+
+		 PreparedStatement pstmt = con.prepareStatement(query);
+		 pstmt.setString(1, email);
+		
+		 
+		 ResultSet resultset= pstmt.executeQuery();
+		 
+		 int count=0;
+		 while (resultset.next() ) {
+			 	count+=1;
+		    	JsonObjectBuilder job = Json.createObjectBuilder();
+		    	double raw= resultset.getInt(2)/360;
+			    int uren = (int) raw;
+			    int minuten = (int) ((raw - uren)*100);
+			    double rawkm = resultset.getInt(3)/1000;
+			    double round = (int)rawkm*10 /10;
+			 	job.add("ritnaam", resultset.getString(1));
+			 	job.add("keer gereden",resultset.getInt(4));
+			 	job.add("totaalkm", String.valueOf(round));
+			 	job.add("totaaltijd", "Totale tijd: "+Integer.toString(uren)+" uur en "+Integer.toString(minuten)+".");
+			 	
+		    	jab.add(job);
+			 
+		 } 
+			 
+		 JsonArray array = jab.build();
+		 return array;
+		 }
+		 
+		 catch(SQLException e)
+		 {
+		 e.printStackTrace();
+		 }
+		 JsonObjectBuilder job = Json.createObjectBuilder();
+		 job.add("0", "Failure");
+		 JsonArray array = jab.build();
+		 return array;// On failure, send a message from here.
+		 }
+}
---
+++ src/main/java/servlets/DataServlet.java
@@ -0,0 +1,36 @@
+package servlets;
+import java.io.IOException;
+import java.text.ParseException;
+
+import javax.json.JsonArray;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.dataDAO;
+
+public class DataServlet extends HttpServlet {  
+	private static final long serialVersionUID = 1L;
+	  public DataServlet() {
+	    	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                    throws ServletException, IOException {  
+    	response.setContentType("application/json");
+    	response.setCharacterEncoding("UTF-8");
+    	
+    	String start = request.getParameter("start");
+    	String eind = request.getParameter("end");
+    	String email = request.getParameter("email");
+    	
+    	dataDAO dataDAO = new dataDAO();
+    	  
+    	JsonArray getData = null;
+		getData = dataDAO.getData(email);
+        response.getWriter().write(getData.toString());
+    	
+    	
+    }
+    
+
+}
