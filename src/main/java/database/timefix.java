d9c1a2e8a518cceb69bee3f51b5dba867e8c2e98
Tim <stellager@users.noreply.github.com>
2017-06-21 11:27:29 +0200

    ss

--- src/main/java/database/timefix.java
+++ src/main/java/database/timefix.java
@@ -8,8 +8,11 @@ public String fixedTime(String time){
 	}
 	else{
 		String newtime2 = time.substring(0, (time.indexOf("P")-1));
-		 int i =Integer.parseInt(newtime2);
+		String newtime_right = newtime2.substring(newtime2.indexOf(":"));
+		String newtime_left = newtime2.substring(0, newtime2.indexOf(":"));
+		int i = Integer.parseInt(newtime_left);
 		String newtime = Integer.toString(i+12);
+		newtime+=newtime_right;
 		return newtime;
 	}
 }
