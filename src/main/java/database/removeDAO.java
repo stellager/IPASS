2f1040b81c522497e6367ac204c843895eb23004
Tim <stellager@users.noreply.github.com>
2017-06-22 02:35:53 +0200

    delete

--- src/main/java/database/removeDAO.java
+++ src/main/java/database/removeDAO.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class removeDAO extends BaseDAO {
-	public String loginUser(String id)
+	public String removeRit(String id)
 	{
 		String query = "DELETE FROM ritten WHERE ritid = ?";
 
---
+++ src/main/java/servlets/DeleteServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.removeDAO;
+
+public class DeleteServlet {
+	private static final long serialVersionUID = 1L;
+	  public DeleteServlet() {
+	    	}
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+                  throws ServletException, IOException {  
+  	
+  	
+        
+      String id=request.getParameter("id");  
+    
+      removeDAO  removeDAO  = new  removeDAO();
+
+    String userLoggedIn = removeDAO.removeRit(id);
+      if(userLoggedIn.contains("SUCCESS"))   
+      {
+      	
+    	  response.setStatus(HttpServletResponse.SC_OK);
+      	 
+      	
+      }
+      else   //On Failure, display a meaningful message to the User.
+      {
+      	
+      }
+    
+  }  
+}
