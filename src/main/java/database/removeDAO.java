d077a0bbb510e79860d540629d2575cd9beaba17
Tim <stellager@users.noreply.github.com>
2017-06-22 02:25:46 +0200

    a

---
+++ src/main/java/database/removeDAO.java
@@ -0,0 +1,39 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class removeDAO extends BaseDAO {
+	public String loginUser(String id)
+	{
+		String query = "DELETE FROM ritten WHERE ritid = ?";
+
+		 try (Connection con = super.getConnection()) {
+
+		 PreparedStatement pstmt = con.prepareStatement(query);
+		 pstmt.setString(1, id);
+		
+		 
+		 ResultSet resultset= pstmt.executeQuery();
+		 
+		 if (resultset.next() ) {
+			 
+			 return "SUCCESS"+resultset.getString(2);
+		 }else{
+			 
+		 return "NOUSER"; 
+		 
+		 }}
+		 
+		 catch(SQLException e)
+		 {
+		 e.printStackTrace();
+		 }
+		 
+		 return "Failure";  // On failure, send a message from here.
+		 }
+	
+
+}
